53a69a072f1c544d9b293d6d82e10221ca8607aa
chinthaashok007 <chinthaashok007@gmail.com>
2025-11-13 16:23:23 +0530

    Add admin URL and include demo URLs

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', include('demo.urls')),  # 👈 replace with your app folder name
 ]
