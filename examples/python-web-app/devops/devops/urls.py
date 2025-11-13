d6ac84c775689f8153dd55a2d3dd3e1061221af7
chinthaashok007 <chinthaashok007@gmail.com>
2025-11-13 16:35:55 +0530

    Update URL patterns to include admin and demo app

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path, include
 
-def index(request):
-    return render(request, 'demo_site.html')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('demo.urls')),  # 👈 Replace 'demo' with your app’s name
+]
