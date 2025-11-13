ace137c8dde92ac6f87e398b4d0bb920be41d0fd
chinthaashok007 <chinthaashok007@gmail.com>
2025-11-13 16:36:39 +0530

    Update URL patterns to use views instead of admin

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('demo.urls')),  # 👈 replace with your app folder name
+    path('', views.index, name='index'),
 ]
